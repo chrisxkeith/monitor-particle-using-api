6ecdf8c6c071f5482e854f522fba5b77ae657bdd
chrisxkeith <chris.keith@gmail.com>
2018-07-10 12:35:09 -0700

    Clean up instructions. Better error checking.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,29 @@
 #! /bin/bash
-#set -x
+# Select all, copy to temporary shell script in temporary directory, then run it.
+# After a couple of minutes, you should see output something like:
+# 2018-07-10T12:27:52-07  someone@somewhere.io     PhotonMonitor thread starting.
+# ...
+# Use Ctrl-C (Cmd-C? on mac) to quit.
+
+mvn --version
+if [ $? -ne 0 ] ; then
+	echo "Please install maven."
+	exit -2
+fi
+echo ""
+
+if [ ! -f ~/Documents/particle-tokens.txt ] ; then
+	echo "Please create ~/Documents/particle-tokens.txt containing one or more lines with <token><tab><accountName>"
+	exit -2
+fi
 
 if [ -d ~/Documents/Github/monitor-particle-using-api ] ; then
 	echo "Please rename ~/Documents/Github/monitor-particle-using-api"
 	exit -2
 fi
 
-if [ ! -f ~/Documents/particle-tokens.txt ] ; then
-	echo "Please create ~/Documents/particle-tokens.txt containing tokens."
-	exit -2
-fi
-
+set -x
+mkdir -p ~/Documents/Github												; if [ $? -ne 0 ] ; then exit -6 ; fi
 cd ~/Documents/Github													; if [ $? -ne 0 ] ; then exit -6 ; fi
 rm -rf JParticle/														; if [ $? -ne 0 ] ; then exit -6 ; fi
 git clone https://github.com/Walter-Stroebel/JParticle.git				; if [ $? -ne 0 ] ; then exit -6 ; fi
@@ -20,13 +33,3 @@ cp -R JParticle/src/* monitor-particle-using-api/src/					; if [ $? -ne 0 ] ; th
 cd ~/Documents/Github/monitor-particle-using-api	  					; if [ $? -ne 0 ] ; then exit -6 ; fi
 
 mvn clean install exec:java -Dexec.mainClass="com.ckkeith.monitor.Main"	; if [ $? -ne 0 ] ; then exit -6 ; fi
-exit 0
-
-After a couple of minutes, you should see output something like:
-...
-Jun 06, 2018 9:25:52 AM com.ckkeith.monitor.PhotonMonitor run
-INFO: Logging to C:\Users\chris\Documents\tmp\CK_particle_log.txt
-2018-06-06T09:25:52-07  CK : PhotonMonitor thread starting up.
-2018-06-06T09:25:52-07  thermistor-test         1c002c001147343438323536        false   Tue May 29 17:05:00 PDT 2018    unknown (no variables)
-...
-Use Ctrl-C (Cmd-C? on mac) to quit (if-and-when you want).
