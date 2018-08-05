9be0925991183af72849d128f29200433ea79349
chrisxkeith <chris.keith@gmail.com>
2018-08-05 14:48:33 -0700

    Try self-shutdown (and restart) to kludge around the random times it stops monitoring.

--- src/main/java/com/ckkeith/monitor/Main.java
+++ src/main/java/com/ckkeith/monitor/Main.java
@@ -2,6 +2,7 @@
 package com.ckkeith.monitor;
 
 import java.io.PrintStream;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Main {
@@ -14,6 +15,12 @@ public class Main {
 				}
 			}
 			Utils.logToConsole(GMailer.sendSubjectLine(Utils.nowInLogFormat() + " " + "Particle Monitor server started on " + Utils.getHostName()));
+
+			// At midnight (local time), shut down. Task Scheduler (on Windows) will start a new instance at 12:05.
+//			LocalDateTime then = LocalDateTime.now().plusMinutes(2); // for testing self-shutdown only.
+			LocalDateTime then = LocalDateTime.now().withHour(23).withMinute(59).withSecond(0);
+			Utils.sleepUntil("MonitorParticle main", then);
+			System.exit(0);
 		} catch (Exception e) {
 			Utils.logToConsole("main() :\t" + e.getClass().getName() + "\t" + e.getMessage());
 			e.printStackTrace(new PrintStream(System.out));
