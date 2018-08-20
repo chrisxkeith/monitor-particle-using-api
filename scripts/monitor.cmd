32b12ebeb25d643439651112de104e120a687b84
chrisxkeith <chris.keith@gmail.com>
2018-08-20 09:10:00 -0700

    Fix usage of file containing return char.

--- scripts/monitor.cmd
+++ scripts/monitor.cmd
@@ -2,10 +2,11 @@ rem ----- Can be called from Task Scheduler
 
 set h=%HOMEDRIVE%%HOMEPATH%
 mkdir %h%\Documents\tmp\
-set o=%h%\Documents\tmp\ret.ret
-date < %o%
-time < %o%
-set >> %o% 2>&1 
+set r=%h%\Documents\tmp\ret.ret
+date < %r%
+time < %r%
+set o=%h%\Documents\tmp\monitor.log
+set >> %o% 2>&1
 where mvn >> %o% 2>&1 
 cd %h%\Documents\Github\monitor-particle-using-api >> %o% 2>&1 
 mvn clean install exec:java -Dexec.mainClass="com.ckkeith.monitor.Main" >> %o% 2>&1 
