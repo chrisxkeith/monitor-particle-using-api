2048aa0da921d21a1cf498b9c4081ded342cadc5
chrisxkeith <chris.keith@gmail.com>
2018-09-26 09:49:22 -0700

    Clean up order of CMDs.

--- scripts/monitor.cmd
+++ scripts/monitor.cmd
@@ -1,13 +1,13 @@
 rem ----- You may have to copy this script to a directory that is public to all users
 rem ----- to make sure that it can be executed by Task Scheduler.
-rem ----- Or you might have to replace %HOMEDRIVE%%HOMEPATH% with hardcoded values.
+rem ----- Or you might have to replace %HOMEDRIVE%%HOMEPATH% with hard coded values.
 rem ----- TODO : create tmp/ and put ret.ret file into it.
 
 set h=%HOMEDRIVE%%HOMEPATH%
-set r=%h%\Documents\tmp\ret.ret
-date < %r%
-time < %r%
 set o=%h%\Documents\tmp\monitor.log
+set r=%h%\Documents\tmp\ret.ret
+date < %r% >> %o% 2>&1
+time < %r% >> %o% 2>&1
 set >> %o% 2>&1
 where mvn >> %o% 2>&1 
 cd %h%\Documents\Github\monitor-particle-using-api >> %o% 2>&1 
