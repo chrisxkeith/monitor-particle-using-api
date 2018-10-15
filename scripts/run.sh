3d34551793d8632fb1f81c6729b6fe3fe27acb91
chrisxkeith <chris.keith@gmail.com>
2018-10-15 14:21:49 -0700

    Add some error checking.

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,6 +9,6 @@ cd ~/Documents/Github/monitor-particle-using-api/
 account=chris-keith-gmail-com
 set -x
 rm -f ~/Documents/tmp/${account}/*.html
-
+															  if [ $? -ne 0 ] ; then exit -6 ; fi
 mvn clean install exec:java -Dmaven.test.skip=true -Dexec.mainClass="com.ckkeith.monitor.Main" | \
  tee -a ~/Documents/tmp/monitor.log
