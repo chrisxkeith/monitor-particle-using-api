344cf996793c3bbfaf4fb0d356a1578916a1c921
CK <chris.keith@gmail.com>
2018-12-03 08:34:52 -0800

    Refactor code into GServicesProvider.

--- src/main/java/com/ckkeith/monitor/GMailer.java
+++ src/main/java/com/ckkeith/monitor/GMailer.java
@@ -3,7 +3,6 @@ package com.ckkeith.monitor;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
@@ -16,17 +15,7 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.Base64;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.gmail.model.Label;
@@ -34,42 +23,14 @@ import com.google.api.services.gmail.model.ListLabelsResponse;
 import com.google.api.services.gmail.model.Message;
 
 public class GMailer {
-	private static final String APPLICATION_NAME = "Gmail API Java Quickstart";
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-	private static final String CREDENTIALS_FOLDER = "credentials"; // Directory to store user credentials.
-	private static NetHttpTransport HTTP_TRANSPORT;
 	private static Gmail service;
-
 	/**
-	 * Global instance of the scopes required by this quickstart. If modifying these
-	 * scopes, delete your previously saved credentials/ folder.
+	 * If modifying these scopes, delete your previously saved credentials/ folder.
 	 */
 	private static final List<String> SCOPES = Arrays.asList(GmailScopes.GMAIL_LABELS, GmailScopes.GMAIL_COMPOSE,
 			GmailScopes.GMAIL_INSERT, GmailScopes.GMAIL_MODIFY);
 
 	private static final String CLIENT_SECRET_DIR = "client_secret.json";
-
-	/**
-	 * Creates an authorized Credential object.
-	 * 
-	 * @param HTTP_TRANSPORT
-	 *            The network HTTP Transport.
-	 * @return An authorized Credential object.
-	 * @throws IOException
-	 *             If there is no client_secret.
-	 */
-	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-		// Load client secrets.
-		InputStream in = GMailer.class.getResourceAsStream(CLIENT_SECRET_DIR);
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-				clientSecrets, SCOPES)
-						.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(CREDENTIALS_FOLDER)))
-						.setAccessType("offline").build();
-		return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-	}
 
 	/**
 	 * Create a MimeMessage using the parameters provided.
@@ -146,11 +107,8 @@ public class GMailer {
 
 	private static Gmail getService() throws Exception {
 		if (service == null) {
-			// Build a new authorized API client service.
-			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-			service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-					.setApplicationName(APPLICATION_NAME).build();
-
+			InputStream in = GMailer.class.getResourceAsStream(CLIENT_SECRET_DIR);
+			service = GServicesProvider.getGmailService(SCOPES, in);
 		}
 		return service;
 	}
