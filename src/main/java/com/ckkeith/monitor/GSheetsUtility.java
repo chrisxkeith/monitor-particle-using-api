016662a1b29c63a6bd4681500d97ea4284b41661
CK <chris.keith@gmail.com>
2018-12-05 08:35:15 -0800

    GSheetsUtility integrated and tested.

--- src/main/java/com/ckkeith/monitor/GSheetsUtility.java
+++ src/main/java/com/ckkeith/monitor/GSheetsUtility.java
@@ -38,7 +38,7 @@ public class GSheetsUtility {
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
 	private static final List<String> SCOPES = Arrays.asList(SheetsScopes.DRIVE, SheetsScopes.SPREADSHEETS);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    private static final String CREDENTIALS_FILE_PATH = "credentials.json";
 
     private static Sheets sheetsService;
     
--- src/test/java/com/ckkeith/monitor/SheetsTest.java
+++ src/test/java/com/ckkeith/monitor/SheetsTest.java
@@ -13,30 +13,30 @@ public class SheetsTest extends TestCase {
 	}
 
 	public void testGetData() {
-//		try {
-//			List<List<Object>> values = GSheetsUtility.getRange("1vo9OiDWyDvUGhjaXLUrnr9FkzO2NdUgN-AVe_CONU6U",
-//					"Sheet 1!A1:C10");
-//			GSheetsUtility.printData(values);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			fail();
-//		}
+		try {
+			List<List<Object>> values = GSheetsUtility.getRange("1vo9OiDWyDvUGhjaXLUrnr9FkzO2NdUgN-AVe_CONU6U",
+					"Sheet1!A1:C10");
+			GSheetsUtility.printData(values);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
-	
 
 	String doTestCreate() throws Exception {
 		String t = LocalDateTime.now().toString();
 		String name = "test sheet " + t;
 		String spreadSheetId = GSheetsUtility.create(name);
 		System.out.println("created sheet named: " + name + " with spreadSheetId: " + spreadSheetId);
-		System.out.println("Sleeping for 10 seconds.");
+// Uncomment for manual verification
+//		System.out.println("Sleeping for 10 seconds.");
 		Thread.sleep(10 * 1000);
 		return spreadSheetId;
 	}
 
-	Integer doTestAppend(String spreadSheetId) throws Exception {
+	Integer doTestAppend(String spreadSheetId, int nRows) throws Exception {
 		Integer i;
-		for (i = 1; i <= 10; i++) {
+		for (i = 1; i <= nRows; i++) {
 			List<List<Object>> values = Arrays.asList(Arrays.asList((Object) ("value A " + i), (Object) ("value B " + i)));
 			String targetCell = "A" + i;
 			GSheetsUtility.appendData(spreadSheetId, targetCell, values);
@@ -48,17 +48,17 @@ public class SheetsTest extends TestCase {
 	}
 
 	void doTestDelete(String spreadSheetId, Integer rowToStart) throws Exception {
-		for (Integer i = rowToStart; i > 1; i--) {
+		for (Integer i = rowToStart; i > 0; i--) {
 			GSheetsUtility.deleteRow(spreadSheetId, i - 1, 1);
 			List<List<Object>>  values = GSheetsUtility.getRange(spreadSheetId, "Sheet1!A1:B10");
-			System.out.println("Deleted (I hope) row at " + i);
+			System.out.println("Deleted (I hope) row " + i);
 			GSheetsUtility.printData(values);
 		}
 	}
 
-	void xtestAll() throws Exception {
+	public void testAll() throws Exception {
 		String spreadSheetId = doTestCreate();
-		Integer i = doTestAppend(spreadSheetId);
+		Integer i = doTestAppend(spreadSheetId, 2);
 		doTestDelete(spreadSheetId, i);
 	}
 }
