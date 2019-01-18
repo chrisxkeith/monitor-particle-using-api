5560c81781537e50a45c505dd16de24143d03dbe
CK <chris.keith@gmail.com>
2019-01-18 14:19:38 -0800

    Remove unused code.

--- src/main/java/com/ckkeith/monitor/RunParams.java
+++ src/main/java/com/ckkeith/monitor/RunParams.java
@@ -57,44 +57,6 @@ public class RunParams {
 		return rp;
 	}
 
-	static RunParams load(String filePath) {
-		RunParams rp = new RunParams();
-		try {
-			File f = new File(filePath);
-			if (!f.exists()) {
-				Utils.logToConsole("No param file (" + filePath + ") using defaults");
-			} else {
-				Utils.logToConsole("Reading param file : " + filePath);
-				ArrayList<String> params = Utils.readParameterFile(filePath);
-				for (String s : params) {
-					String p[] = s.split("=");
-					if (p.length > 1) {
-						String key = p[0].trim();
-						String val = p[1].trim();
-						if (key.equalsIgnoreCase("dataIntervalInMinutes")) {
-							rp.dataIntervalInMinutes = Integer.valueOf(val);
-						} else if (key.equalsIgnoreCase("htmlWriteIntervalInSeconds")) {
-							rp.htmlWriteIntervalInSeconds = Integer.valueOf(val);
-						} else if (key.equalsIgnoreCase("expectedEventRateInSeconds")) {
-							rp.expectedEventRateInSeconds = Integer.valueOf(val);
-						} else if (key.equalsIgnoreCase("devicesToReport")) {
-							rp.devicesToReport = val;
-						} else if (key.equalsIgnoreCase("deviceNameToSheetId")) {
-							rp.deviceNameToSheetId = val;
-						} else if (key.equalsIgnoreCase("csvTimeGranularityInSeconds")) {
-							rp.csvTimeGranularityInSeconds = Integer.valueOf(val);
-						} else if (key.equalsIgnoreCase("sheetsWriteIntervalInSeconds")) {
-							rp.sheetsWriteIntervalInSeconds = Integer.valueOf(val);
-						}
-					}
-				}				
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return rp;
-	}
-
 	public String toString() {
 		return "RunParams : "
 				+ "dataIntervalInMinutes = " + dataIntervalInMinutes
