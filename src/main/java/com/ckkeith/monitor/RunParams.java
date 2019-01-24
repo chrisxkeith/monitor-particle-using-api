2a220c41ac156b27feee1ddbc8f98f20855b3fd5
CK <chris.keith@gmail.com>
2019-01-24 08:36:01 -0800

    Sheet-based XML is parsed without errors.

--- src/main/java/com/ckkeith/monitor/RunParams.java
+++ src/main/java/com/ckkeith/monitor/RunParams.java
@@ -1,18 +1,33 @@
 // Please credit chris.keith@gmail.com
 package com.ckkeith.monitor;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 
 public class RunParams {
+
+	public class Dataset {
+		// microcontrollerName -> list of sensor names
+		Hashtable<String, HashSet<String>> microcontrollers;
+		public Dataset(Hashtable<String, HashSet<String>> microcontrollers) {
+			this.microcontrollers = microcontrollers;
+		}
+	};
+
 	Integer		dataIntervalInMinutes = 10;
 	Integer		htmlWriteIntervalInSeconds = 5;
 	Integer		expectedEventRateInSeconds = 60 * 2;
 	String		devicesToReport = "";
 	String		deviceNameToSheetId = "";
-	Integer		csvTimeGranularityInSeconds = 60;	// E.g. '60' == round to minutes, '30' == round to minutes and half-minutes.
+	Integer		csvTimeGranularityInSeconds = 60; // E.g. '60' == round to minutes, '30' == round to minutes and half-minutes.
 	Integer		sheetsWriteIntervalInSeconds = 10;
-	
+	Hashtable<String, ArrayList<Dataset>> sheets =
+				new Hashtable<String, ArrayList<Dataset>>();
+
 	private static Integer getInteger(Element root, String name, Integer defaultValue) {
 		NodeList nl = root.getElementsByTagName(name);
 		if (nl.getLength() > 0) {
@@ -41,6 +56,63 @@ public class RunParams {
 		return ret;
 	}
 
+	private ArrayList<Dataset> buildDatasetList(NodeList datasetElems) throws Exception {
+		ArrayList<Dataset> datasets = new ArrayList<Dataset>();
+		for (int i = 0; i < datasetElems.getLength(); i++) {
+			Node datasetNode = datasetElems.item(i);
+			if (datasetNode.getNodeType() == Node.ELEMENT_NODE) {
+				Hashtable<String, HashSet<String>> microcontrollers = new Hashtable<String, HashSet<String>>();
+				Element elem = (Element)datasetNode;
+				NodeList microcontrollerNameElems = elem.getElementsByTagName("microcontrollerName");
+				if (microcontrollerNameElems.getLength() == 0) {
+					throw new Exception("microcontrollerName.getLength() == 0");
+				}
+				String microcontrollerName = microcontrollerNameElems.item(0).getTextContent();
+				NodeList sensors = elem.getElementsByTagName("sensors");
+				if (sensors.getLength() == 0) {
+					throw new Exception("sensors.getLength() == 0");
+				}
+				HashSet<String> sensorNames = new HashSet<String>();
+				for (int nameIndex = 0; nameIndex < sensors.getLength(); nameIndex++) {
+					sensorNames.add(sensors.item(nameIndex).getTextContent());
+				}
+				microcontrollers.put(microcontrollerName, sensorNames);
+				datasets.add(new Dataset(microcontrollers));
+			}
+		}
+		return datasets;
+	}
+
+	private void addSheet(Element sheetElement) throws Exception {
+		NodeList sheetIdList = sheetElement.getElementsByTagName("sheetId");
+		if (sheetIdList.getLength() == 0) {
+			throw new Exception("sheetIdList.getLength() == 0");
+		}
+		String sheetId = sheetIdList.item(0).getTextContent();
+		NodeList datasetElems = sheetElement.getElementsByTagName("dataSet");
+		if (datasetElems.getLength() == 0) {
+			throw new Exception("datasets.getLength() == 0");
+		}
+		sheets.put(sheetId, buildDatasetList(datasetElems));
+	}
+
+	private void loadSheets(Element root) throws Exception {
+		NodeList sheetsRoot = root.getElementsByTagName("sheets");
+		if (sheetsRoot.getLength() == 0) {
+			throw new Exception("sheetsRoot.getLength() == 0");
+		}
+		Node sheets = sheetsRoot.item(0);
+		if (sheets.getNodeType() != Node.ELEMENT_NODE) {
+			throw new Exception("sheets.getNodeType() != Node.ELEMENT_NODE");
+		}
+		NodeList sheetsList = ((Element)sheets).getElementsByTagName("sheet");
+		for (int i = 0; i < sheetsList.getLength(); i++) {
+			if (sheetsList.item(i).getNodeType() == Node.ELEMENT_NODE) {
+				addSheet((Element)sheetsList.item(i));
+			}
+		}
+	}
+
 	static RunParams loadFromXML(String filePath) throws Exception {
 		RunParams rp = new RunParams();
 		Element root = Utils.readTextFileIntoDOM(filePath).getDocumentElement();
@@ -51,6 +123,7 @@ public class RunParams {
 		rp.sheetsWriteIntervalInSeconds = getInteger(root, "sheetsWriteIntervalInSeconds", rp.sheetsWriteIntervalInSeconds);
 		rp.devicesToReport = getString(root, "devicesToReport", rp.devicesToReport);
 		rp.deviceNameToSheetId = getDeviceList(root);
+		rp.loadSheets(root);
 		return rp;
 	}
 
