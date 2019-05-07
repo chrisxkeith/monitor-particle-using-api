75c8acc7d49f2e48cc9de34a4945a9cbe49af929
CK <chris.keith@gmail.com>
2019-05-07 07:37:23 -0700

    Delete failed attempt at run.cmd

--- scripts/run.cmd
+++
@@ -1,7 +0,0 @@
-rem : Not fully tested...
-pushd %HOMEDRIVE%%HOMEPATH%\Documents\Github\monitor-particle-using-api\
-                                                        	IF %ERRORLEVEL% NEQ 0 goto :theend
-mkdir -p %HOMEDRIVE%%HOMEPATH%\Documents\tmp\%HOSTNAME%
-                                                        	IF %ERRORLEVEL% NEQ 0 goto :theend
-powershell "mvn clean install exec:java -Dmaven.test.skip=true -Dexec.mainClass=`"com.ckkeith.monitor.Main`" | ^
-	tee -a %HOMEDRIVE%%HOMEPATH%\Documents\tmp\%HOSTNAME%\monitor.log"
