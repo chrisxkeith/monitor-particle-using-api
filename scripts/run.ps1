135fa144dd5b9af2ee6939d6ad8db3bbadda67a3
CK <chris.keith@gmail.com>
2019-05-11 09:43:54 -0700

    Get basic mvn command working.

---
+++ scripts/run.ps1
@@ -0,0 +1,14 @@
+# run.sh seems to (sometimes) leave processes running.
+# Try Powershell to see if it works better.
+
+Set-PSDebug -Trace 1
+Push-Location $Env:HOMEDIR$Env:HOMEPATH\Documents\Github\monitor-particle-using-api\
+
+# TODO : Check exit statuses
+# TODO : New-Item -path $Env:HOMEDIR$Env:HOMEPATH\Documents\tmp\$Env:HOSTNAME
+
+mvn clean install exec:java "`-Dmaven.test.skip=true" "`-Dexec.mainClass=`"com.ckkeith.monitor.Main`""
+
+# TODO : | Tee-Object -Append $Env:HOMEDIR$Env:HOMEPATH\Documents\tmp\$Env:HOSTNAME\monitor.log
+
+Pop-Location
