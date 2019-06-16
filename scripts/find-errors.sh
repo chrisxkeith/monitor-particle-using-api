a19b3e94e12ceed8c6ca62351be06cd8b3279a20
CK <chris.keith@gmail.com>
2019-06-16 09:51:50 -0700

    Get startup times and exceptions from monitor.log

--- scripts/find-errors.sh
+++ scripts/find-errors.sh
@@ -1,10 +1,5 @@
-# Must first use File Explorer to show file system of remote machine.
-
 set -x
 
-mkdir -p ~/Documents/tmp/2018-CK-NUC/                           		    ; if [ $? -ne 0 ] ; then exit -6 ; fi
-cd ~/Documents/tmp/2018-CK-NUC/							                    ; if [ $? -ne 0 ] ; then exit -6 ; fi
-# cp '\\2018-CK-NUC\Users\chris\Documents\tmp\2018-CK-NUC\monitor01.log' .    ; if [ $? -ne 0 ] ; then exit -6 ; fi
-grep -n xception monitor01.log | tail -20
-cp '\\2018-CK-NUC\Users\chris\Documents\tmp\2018-CK-NUC\monitor.log' .	    ; if [ $? -ne 0 ] ; then exit -6 ; fi
-grep -n xception monitor.log | tail -20
+cd ~/Documents/tmp/${HOSTNAME}/							                    ; if [ $? -ne 0 ] ; then exit -6 ; fi
+grep xception monitor*.log
+grep 'AccountMonitor thread starting' monitor*.log
