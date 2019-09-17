8466aa722718efec24066e11a36606a13c1059a7
CK <chris.keith@gmail.com>
2019-09-17 15:27:46 -0700

    Echo more to log for debugging.

--- scripts/monitor.cmd
+++ scripts/monitor.cmd
@@ -10,7 +10,9 @@ echo %DATE% %TIME% >> %log%
 set >> %log%
 
 pushd %userhome%\Documents\Github\monitor-particle-using-api
+echo About to git pull >> %log%
 git pull 2>&1 >> %log%
+echo About call mvn install... >> %log%
 call mvn clean install exec:java -D maven.test.skip=true -D exec.mainClass="com.ckkeith.monitor.Main" 2>&1 >> %log%
 echo %DATE% %TIME% >> %log%
 echo mvn clean install... finished >> %log%
